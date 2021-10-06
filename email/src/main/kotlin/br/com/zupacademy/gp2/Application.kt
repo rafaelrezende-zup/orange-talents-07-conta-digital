113fd31ea859daa02452f9d08fba7eea06ab1d2d
rafael.rezende <rafael.rezende@zup.com.br>
2021-10-06 17:52:59 -0300

    Implementação comsumidor kafka para Email

---
+++ email/src/main/kotlin/br/com/zupacademy/gp2/Application.kt
@@ -0,0 +1,11 @@
+package br.com.zupacademy.gp2
+
+import io.micronaut.runtime.Micronaut.build
+
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("br.com.zupacademy.gp2")
+		.start()
+}
+
